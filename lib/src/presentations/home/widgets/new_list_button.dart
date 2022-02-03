fb4e97b3bb9d82d148462262895c06607886dc60
gonzalo.gauto <gonzalo.gauto@gmail.com>
2022-02-03 20:16:28 -0300

    organize imports

--- lib/src/presentations/home/widgets/new_list_button.dart
+++ lib/src/presentations/home/widgets/new_list_button.dart
@@ -1,7 +1,7 @@
-import '../../lists/cubit/shopping_list_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../lists/cubit/shopping_list_cubit.dart';
 import 'add_list_dialog.dart';
 
 class NewListButton extends StatelessWidget {
